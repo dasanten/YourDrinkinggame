bb3d8062f6e55ad54039f1dbb98ff2dc5c58181e
dasanten <daniel-sa@t-online.de>
2020-02-24 17:29:58 +0100

    initial

---
+++ backend/src/test/java/de/dasanten/YourDrinkgame/YourDrinkgameApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dasanten.YourDrinkgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourDrinkgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
