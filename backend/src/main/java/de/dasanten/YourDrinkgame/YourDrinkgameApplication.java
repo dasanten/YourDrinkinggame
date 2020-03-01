ae45929770d4398b72f15a0d32690c59001c52e2
Daniel Santen <daniel-sa@t-online.de>
2020-03-01 16:06:24 +0100

    initial

---
+++ backend/src/main/java/de/dasanten/YourDrinkgame/YourDrinkgameApplication.java
@@ -0,0 +1,13 @@
+package de.dasanten.YourDrinkgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourDrinkgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourDrinkgameApplication.class, args);
+	}
+
+}
