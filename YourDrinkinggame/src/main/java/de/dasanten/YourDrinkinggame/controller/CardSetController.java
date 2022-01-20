a5dbeb868c24b49134d8be2eb8e85e057288e15f
dasanten <github@dasanten.de>
2022-01-20 21:17:01 +0100

    initial new backend commit

---
+++ YourDrinkinggame/src/main/java/de/dasanten/YourDrinkinggame/controller/CardSetController.java
@@ -0,0 +1,12 @@
+package de.dasanten.YourDrinkinggame.controller;
+
+import de.dasanten.YourDrinkinggame.api.CardsetApi;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Controller
+@AllArgsConstructor
+@CrossOrigin
+public class CardSetController implements CardsetApi {
+}
