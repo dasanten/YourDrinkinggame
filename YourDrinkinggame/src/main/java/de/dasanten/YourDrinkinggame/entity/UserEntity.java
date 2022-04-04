2ff0774584b2c0031c383fb1b24c99deae0bf50f
dasanten <github@dasanten.de>
2022-04-04 22:56:20 +0200

    fixed feedback just for owner adn admins

--- YourDrinkinggame/src/main/java/de/dasanten/YourDrinkinggame/entity/UserEntity.java
+++ YourDrinkinggame/src/main/java/de/dasanten/YourDrinkinggame/entity/UserEntity.java
@@ -1,14 +1,11 @@
 package de.dasanten.YourDrinkinggame.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import de.dasanten.YourDrinkinggame.entity.enums.UserRole;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
-import javax.persistence.Entity;
 import java.util.List;
 
 @Entity(name = "user")
--- YourDrinkinggame/src/main/java/de/dasanten/YourDrinkinggame/service/FeedbackService.java
+++ YourDrinkinggame/src/main/java/de/dasanten/YourDrinkinggame/service/FeedbackService.java
@@ -1,20 +1,27 @@
 package de.dasanten.YourDrinkinggame.service;
 
 import de.dasanten.YourDrinkinggame.entity.FeedbackEntity;
+import de.dasanten.YourDrinkinggame.entity.UserEntity;
+import de.dasanten.YourDrinkinggame.entity.enums.UserRole;
+import de.dasanten.YourDrinkinggame.exception.custom.MissingPermissionException;
 import de.dasanten.YourDrinkinggame.mapper.FeedbackMapper;
 import de.dasanten.YourDrinkinggame.model.FeedbackDto;
 import de.dasanten.YourDrinkinggame.repository.FeedbackRepository;
+import de.dasanten.YourDrinkinggame.repository.UserRepository;
+import de.dasanten.YourDrinkinggame.security.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class FeedbackService {
 
     private final FeedbackRepository feedbackRepository;
+    private final UserRepository userRepository;
 
     private final FeedbackMapper feedbackMapper;
 
@@ -24,6 +31,15 @@ public class FeedbackService {
     }
 
     public List<FeedbackDto> getUserFeedback() {
+        Optional<UserEntity> userEntityOptional = userRepository.findById(SecurityUtil.getAuthId());
+        if (userEntityOptional.isEmpty()) {
+            throw new IllegalArgumentException("User does not exist");
+        }
+        UserEntity user = userEntityOptional.get();
+        if (!(user.getRole().equals(UserRole.SUPER_ADMIN) || user.getRole().equals(UserRole.ADMIN))) {
+            throw new MissingPermissionException("You are not allowed to view feedback");
+        }
+
         List<FeedbackEntity> feedbackEntityList = feedbackRepository.findAll();
         List<FeedbackDto> feedbackDtoList = new ArrayList<>();
         feedbackEntityList.forEach(feedbackEntity -> feedbackDtoList.add(feedbackMapper.toDto(feedbackEntity)));
